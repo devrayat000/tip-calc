f375df1e379429a99d232a789c8e7d4cc92354d0
devrayat000 <2010050@me.buet.ac.bd>
2022-07-12 15:16:01 +0600

    implemented tip button

---
+++ src/lib/components/tip.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let selected: boolean = false;
+	export let percent: number;
+	export let list: number[];
+	export let group: number;
+
+	function selectTip() {
+		group = percent;
+	}
+</script>
+
+<div>
+	<button
+		type="button"
+		class="bg-cyan-very-dark text-white font-bold p-3 w-full rounded-md"
+		class:bg-cyan-dark-grayish={group === percent}
+		on:click={selectTip}
+	>
+		<input type="radio" name="tips" id="tips" class="hidden peer" bind:group value={percent} />
+		{percent}%
+	</button>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import Input from '$lib/components/input.svelte';
+	import Tip from '$lib/components/tip.svelte';
 	import dollar from '$lib/icons/icon-dollar.svg';
+	import person from '$lib/icons/icon-person.svg';
+
+	let tips = [5, 10, 15, 25, 50];
+	let group = 5;
+
+	$: {
+		console.log(group);
+	}
 </script>
 
 <main class="font-mono bg-cyan-very-light-grayish min-h-screen relative">
@@ -9,12 +18,35 @@
 			Splitter
 		</h1>
 	</section>
-	<section class="bg-white rounded-t-3xl p-8 absolute bottom-0 inset-x-0 w-full">
-		<form>
+	<form class="bg-white rounded-t-3xl p-8 absolute bottom-0 inset-x-0 w-full">
+		<section>
 			<label class="text-cyan-dark-grayish" for="total">Bill</label>
 			<Input>
 				<img src={dollar} alt="Dollar Icon" slot="icon" />
 			</Input>
-		</form>
-	</section>
+		</section>
+		<section class="mt-8">
+			<label class="text-cyan-dark-grayish" for="tips">Select Tip %</label>
+			<div class="my-1 grid grid-cols-2 place-items-stretch gap-4">
+				{#each tips as tip (tip)}
+					<Tip {group} list={tips} percent={tip} />
+				{/each}
+			</div>
+		</section>
+		<section class="mt-8">
+			<label class="text-cyan-dark-grayish" for="total">Number of People</label>
+			<Input>
+				<img src={person} alt="Person Icon" slot="icon" />
+			</Input>
+		</section>
+		<section class="-mx-2 mt-8 bg-cyan-very-dark rounded-lg p-6 pt-9">
+			<div class="flex items-center justify-between">
+				<div>
+					<h4 class="text-white">Tip Amount</h4>
+					<h6 class="text-cyan-dark-grayish">/ person</h6>
+				</div>
+				<h3 class="text-primary font-bold text-4xl">$4.27</h3>
+			</div>
+		</section>
+	</form>
 </main>
